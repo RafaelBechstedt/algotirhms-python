a59335284edc35b4b8481aac2bf2f64af204d534
Rafael <bechstedt@hotmail.com>
2023-06-15 18:00:15 -0300

    Req

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    numeros_vistos = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in numeros_vistos:
+            return num
+        numeros_vistos.add(num)
+
+    return False
