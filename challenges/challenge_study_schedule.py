a7f2c4c387b05c16eca42b6d97d505830d8fa558
Rafael <bechstedt@hotmail.com>
2023-06-14 22:38:12 -0300

    Req 01 - add comentário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,5 +8,6 @@ def study_schedule(permanence_period, target_time):
         if not isinstance(start, int) or not isinstance(end, int):
             return None
         if target_time in range(start, end + 1):
+            # Range funciona como start <= target_time < end. Por isso o +1
             count += 1
     return count
