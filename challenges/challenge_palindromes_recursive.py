451d5242e61ed42bae65beafa528c421b76c2070
Rafael <bechstedt@hotmail.com>
2023-06-15 00:39:09 -0300

    Req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+# Verifica se a entrada for vazia
+    if low_index >= high_index:
+        return True
+# Verifica se a palavra tem apenas um caractere ou nenhum
+    if word[low_index] != word[high_index]:
+        return False
+# Verifica se os caracteres nos extremos são iguais
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+# Chamada recursiva para verificar os caracteres internos da palavra
