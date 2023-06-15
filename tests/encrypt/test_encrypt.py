b5ef2b3025764355025b2736619acdb050198c50
Rafael <bechstedt@hotmail.com>
2023-06-14 23:35:07 -0300

    Req 2? Não entendi se passou

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message="Rafael", key="string")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(message=123, key=1)
+    assert encrypt_message(message="Rafael", key=7) == "leafaR"
+    assert encrypt_message(message="Rafael", key=4) == "le_afaR"
+    assert encrypt_message(message="Rafael", key=3) == "faR_lea"
